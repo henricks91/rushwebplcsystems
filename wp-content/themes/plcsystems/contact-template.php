51eb28bff90b22719f7cdbcaa311e4a720d2906e
xenosage143 <jcabigas09@gmail.com>
2015-07-30 23:19:27 +0800

    - added content for contact us page - added contact-template.php

---
+++ wp-content/themes/plcsystems/contact-template.php
@@ -0,0 +1,209 @@
+<?php
+/*
+Template Name: Contact Us Template
+*/
+
+get_header(); ?>
+<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
+<script type="text/javascript">
+(function($) {
+
+/*
+*  render_map
+*
+*  This function will render a Google Map onto the selected jQuery element
+*
+*  @type	function
+*  @date	8/11/2013
+*  @since	4.3.0
+*
+*  @param	$el (jQuery element)
+*  @return	n/a
+*/
+
+function render_map( $el ) {
+
+	// var
+	var $markers = $el.find('.marker');
+
+	// vars
+	var args = {
+		zoom		: 16,
+		center		: new google.maps.LatLng(0, 0),
+		mapTypeId	: google.maps.MapTypeId.ROADMAP
+	};
+
+	// create map	        	
+	var map = new google.maps.Map( $el[0], args);
+
+	// add a markers reference
+	map.markers = [];
+
+	// add markers
+	$markers.each(function(){
+
+    	add_marker( $(this), map );
+
+	});
+
+	// center map
+	center_map( map );
+
+}
+
+/*
+*  add_marker
+*
+*  This function will add a marker to the selected Google Map
+*
+*  @type	function
+*  @date	8/11/2013
+*  @since	4.3.0
+*
+*  @param	$marker (jQuery element)
+*  @param	map (Google Map object)
+*  @return	n/a
+*/
+
+function add_marker( $marker, map ) {
+
+	// var
+	var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
+
+	// create marker
+	var marker = new google.maps.Marker({
+		position	: latlng,
+		map			: map
+	});
+
+	// add to array
+	map.markers.push( marker );
+
+	// if marker contains HTML, add it to an infoWindow
+	if( $marker.html() )
+	{
+		// create info window
+		var infowindow = new google.maps.InfoWindow({
+			content		: $marker.html()
+		});
+
+		// show info window when marker is clicked
+		google.maps.event.addListener(marker, 'click', function() {
+
+			infowindow.open( map, marker );
+
+		});
+	}
+
+}
+
+/*
+*  center_map
+*
+*  This function will center the map, showing all markers attached to this map
+*
+*  @type	function
+*  @date	8/11/2013
+*  @since	4.3.0
+*
+*  @param	map (Google Map object)
+*  @return	n/a
+*/
+
+function center_map( map ) {
+
+	// vars
+	var bounds = new google.maps.LatLngBounds();
+
+	// loop through all markers and create bounds
+	$.each( map.markers, function( i, marker ){
+
+		var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
+
+		bounds.extend( latlng );
+
+	});
+
+	// only 1 marker?
+	if( map.markers.length == 1 )
+	{
+		// set center of map
+	    map.setCenter( bounds.getCenter() );
+	    map.setZoom( 16 );
+	}
+	else
+	{
+		// fit to bounds
+		map.fitBounds( bounds );
+	}
+
+}
+
+/*
+*  document ready
+*
+*  This function will render each map when the document is ready (page has loaded)
+*
+*  @type	function
+*  @date	8/11/2013
+*  @since	5.0.0
+*
+*  @param	n/a
+*  @return	n/a
+*/
+
+$(document).ready(function(){
+
+	$('.acf-map').each(function(){
+
+		render_map( $(this) );
+
+	});
+
+});
+
+})(jQuery);
+</script>
+<div id="main-content" class="fullwidth">
+	<div class="main-banner">
+		<?php echo do_shortcode('[layerslider id="1"]'); ?>
+	</div><!--End of main banner-->
+	<div class="container content-area">
+		<div class="row">
+			<?php if ( function_exists('yoast_breadcrumb') ) {
+			yoast_breadcrumb('<p id="breadcrumbs">','</p>');
+			} ?>
+			<?php if (have_posts()) { ?>
+				<?php while (have_posts()) { the_post(); ?>
+					<article class="post col-md-12 target-content">
+						<h1><?php the_title(); ?></h1>
+						<?php the_content(''); ?>
+					</article>
+				<?php } ?>
+			<?php } else { ?>
+				<article class="page col-md-12">
+					<h1>Nothing Found</h1>
+					<p>Sorry, but you are looking for something that isn't here.</p>
+					<p><a href="<?php echo get_option("home"); ?>">Return to the homepage</a></p>
+				</article>
+			<?php } ?>
+		</div>
+		<div class="row address-map-area">
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<?php $_address = get_field( '_plc_address', $post->ID); 	?>
+					<?php echo $_address ?>
+			</div>
+			<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+				<?php 
+					$location = get_field('_plc_google_map');
+					if( !empty($location) ):
+					?>
+					<div class="acf-map">
+						<div class="marker" data-lat="<?php echo $location['lat']; ?>" data-lng="<?php echo $location['lng']; ?>"></div>
+					</div>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
+</div><!--End of main content-->
+<?php get_footer(); ?>
